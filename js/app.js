81e9fb78797faff242d694bf711c8b90dfc6774d
Ramkanta Pramanik <98093534+ramkanta@users.noreply.github.com>
2023-06-03 08:15:58 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const endDate = "02 June 2023 03:11:00 PM"
+const endDate = "02 June 2024 03:11:00 PM"
 
 document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input")
